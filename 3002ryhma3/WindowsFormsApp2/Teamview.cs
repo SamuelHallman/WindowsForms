847062530956c91e79c7ef0db25f8a4e8856b9f7
SamuelHallman <92924123+SamuelHallman@users.noreply.github.com>
2023-01-31 16:55:36 +0200

    Add files via upload

---
+++ 3002ryhma3/WindowsFormsApp2/Teamview.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Teamview : Form
+    {
+        readonly OleDbConnection connection;
+
+        public Teamview(OleDbConnection connection)
+        {
+            InitializeComponent();
+
+            this.connection = connection;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string contentTopic = textBox1.Text;
+            int priorityStatus = Convert.ToInt32(comboBox1.Text);
+            string query = $"INSERT INTO Teams(Team_name, Project_Priorities) VALUES ('{contentTopic}', {priorityStatus})";
+
+            OleDbCommand cmd = new OleDbCommand(query, connection);
+            cmd.ExecuteNonQuery();
+        }
+
+        private void comboBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Teamview_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'ketterakantaDataSet.Persons' table. You can move, or remove it, as needed.
+            this.personsTableAdapter.Fill(this.ketterakantaDataSet.Persons);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 3002ryhma3/WindowsFormsApp2/task.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp2.Models;
+
+namespace WindowsFormsApp2
+{
+    public partial class task : Form
+    {
+        readonly OleDbConnection connection;
+        readonly TaskButton button;
+
+        public bool WindowIsRaised { get; set; }
+        
+        public task(TaskButton button, OleDbConnection connection)
+        {
+            InitializeComponent();
+
+            this.button = button;
+            this.connection = connection;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!WindowIsRaised)
+            {
+                Teamview TeamviewForm = new Teamview(connection);
+                TeamviewForm.Show(this);
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string contentTopic = textBox2.Text;
+                string query = $"INSERT INTO Tasks(Task_Name, status) VALUES ('{contentTopic}', {contentTopic})"; // Lisättävän datan query
+
+                OleDbCommand cmd = new OleDbCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                e.Handled = true;
+            }
+        }
+    }
+}
